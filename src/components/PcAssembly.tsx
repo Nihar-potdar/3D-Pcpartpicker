95ddb5822e62d24cf56342f1a76c9a13c4cf3596
NiharPotdar <starlord.potdar@gmail.com>
2026-09-18 14:24:11 +0530

    added basic animations to 3D components

--- src/components/PcAssembly.tsx
+++ src/components/PcAssembly.tsx
@@ -1,7 +1,8 @@
 import { Case, GPUModel, Motherboard, PSU } from "@/models/models";
 import { corsair4000DTransforms } from "./configs/corsair4000D";
-import type { ThreeElements } from "@react-three/fiber";
+import { type ThreeElements } from "@react-three/fiber";
 import { useBuildStore } from "@/stores/BuildStore";
+import { InstallAnimation } from "./InstallAnimation";
 
 type PcAssemblyProps = ThreeElements["group"];
 
@@ -13,9 +14,27 @@ export function PcAssembly(props: PcAssemblyProps) {
   return (
     <group {...props}>
       <Case />
-      {motherboard && <Motherboard {...corsair4000DTransforms.motherboard} />}
-      {gpu && <GPUModel {...corsair4000DTransforms.gpu} />}
-      {psu && <PSU {...corsair4000DTransforms.psu} />}
+      {motherboard && (
+        <group key={motherboard.id} {...corsair4000DTransforms.motherboard}>
+          <InstallAnimation offset={[0, 0, 0.5]}>
+            <Motherboard />
+          </InstallAnimation>
+        </group>
+      )}
+      {gpu && (
+        <group key={gpu.id} {...corsair4000DTransforms.gpu}>
+          <InstallAnimation offset={[1, 0, 0]}>
+            <GPUModel />
+          </InstallAnimation>
+        </group>
+      )}
+      {psu && (
+        <group key={psu.id} {...corsair4000DTransforms.psu}>
+          <InstallAnimation offset={[0, -0.4, 0.5]}>
+            <PSU />
+          </InstallAnimation>
+        </group>
+      )}
     </group>
   );
 }
